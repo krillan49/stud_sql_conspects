6c340ff8bcc4bb689c64a967f8610fb59502309c
krillan <pushkinidze49@gmail.com>
2023-12-09 17:53:34 +0300

    dgfdgfdgf

--- 11_podzapros.sql
+++ 11_podzapros.sql
@@ -1,56 +1,72 @@
---                                           Подзапросы
-
+--                                              Подзапросы
 
 -- Подзапрос — это запрос, использующийся в другом SQL запросе. Подзапрос всегда заключён в круглые скобки и обычно выполняется перед основным запросом.
 
-	-- 1. Подзапрос возвращающий 1 строку одного столбца(скалярный подзапрос)
-	-- (может использоваться в различных частях основного SQL запроса, но чаще всего он используется в условиях ограничений выборки с помощью операторов сравнения =, <>, >, <)
-SELECT (SELECT name FROM company LIMIT 1) AS company_name                                                       --#=> вывод единственного значения (названия компании)
-SELECT * FROM FamilyMembers WHERE birthday = (SELECT MAX(birthday) FROM FamilyMembers)                          --#=> вывод самых молодых
-SELECT * FROM Reservations WHERE Reservations.room_id = (SELECT id FROM Rooms ORDER BY price DESC LIMIT 1)      --#=> список всех бронирований самой дорогой комнаты
-SELECT Users.* FROM Users JOIN Rooms ON Rooms.owner_id = Users.id WHERE price = (SELECT MAX(price) FROM Rooms)  --#=> в многотабличном запросе
 
-	-- 2. Подзапросы с несколькими строками и одним столбцом(его нельзя просто использовать с операторами сравнения тк возвращает много строк)
-	-- ALL - можем сравнивать отдельное значение с каждым значением в наборе, полученным подзапросом. Вернёт TRUE, только если все сравнения отдельного значения со значениями в наборе вернут TRUE.
-SELECT 200 > ALL(SELECT price FROM Rooms)                                                                    --#=> все ли комнаты дешевле чем 200
-SELECT DISTINCT name FROM Users JOIN Rooms ON Users.id = Rooms.owner_id WHERE Users.id <> ALL (SELECT DISTINCT user_id FROM Reservations)
-	-- IN - проверяет входит ли конкретное значение в набор значений. В качестве такого набора как раз может использовать подзапрос, возвращающий несколько строк с одним столбцом.
-SELECT id, name FROM departments WHERE id IN (SELECT department_id FROM sales WHERE price > 98)              --#=> так как используется IN то в подзапросе должен быть 1 стобец
-SELECT good_name FROM Goods WHERE good_id NOT IN (SELECT good FROM Payments)                                 --#=> NOT IN
-	-- ANY - имеет схожие поведение c IN но возвращает TRUE(те работает через операторы сравнения), если хотя бы одно сравнение отдельного значения со значением в наборе вернёт TRUE.
-SELECT * FROM Users WHERE id = ANY (SELECT DISTINCT owner_id FROM Rooms WHERE price >= 150)                  --#=> найдёт пользователей, которые владеют хотя бы 1 жилым помещением стоимостью более 150
 
-	-- 3. Многостолбцовые подзапросы - возвращающими несколько столбцов и несколько строк (производные таблицы)
-SELECT * FROM Reservations WHERE (room_id, price) IN (SELECT id, price FROM Rooms)                           --#=> попарно сравнивает значения в основном запросе со значениями в подзапросе
-SELECT * FROM Rooms WHERE (has_tv, has_internet) IN (SELECT has_tv, has_internet FROM Rooms WHERE id=11)     --#=> по одной конкретной строке
+--                                          1-а. Скалярный подзапрос
 
-	-- 4. Коррелированные подзапросы - ссылаются на один или несколько столбцов основного запроса(зависимые, все прошлые 3 варианта были независимы от остального запроса)
-	-- (он выполняется не один раз перед выполнением запроса, в который он вложен, а для каждой строки, которая может быть включена в окончательный результат)
-	-- (использование коррелированных подзапросов может вызвать проблемы с производительностью, особенно если содержащий запрос возвращает много строк)
+-- Скалярный подзапрос - это подзапрос возвращающий 1 строку одного столбца(литерал). Он может использоваться в различных частях основного SQL запроса, но чаще всего он используется в условиях ограничений выборки с помощью операторов сравнения =, <>, >, <
+
+SELECT (SELECT name FROM company LIMIT 1) AS company_name                 -- из той же таблицы, в зоне выбора столбцов
+SELECT * FROM Family WHERE birthday = (SELECT MAX(birthday) FROM Family)  -- из той же таблицы, в зоне WHERE
+SELECT * FROM Res WHERE Res.room_id = (SELECT id FROM Rooms ORDER BY price DESC LIMIT 1) -- подзапрос из другой таблицы без объединения таблиц
+SELECT Users.* FROM Users JOIN Rooms ON Rooms.owner_id = Users.id WHERE price = (SELECT MAX(price) FROM Rooms)  -- в многотабличном запросе
+
+
+
+--                            1-б. Подзапросы возвращающие несколько строк одного столбца
+
+-- Подзапросы возвращающие несколько строк одного столбца нельзя просто использовать с операторами сравнения тк они возвращает много строк, поэтому нужно использовать операторы ALL, IN, ANY
+
+-- ALL - сравнивает отдельное значение с каждым значением в наборе, полученным подзапросом. Вернёт TRUE, только если все сравнения отдельного значения со значениями в наборе вернут TRUE.
+SELECT 200 > ALL(SELECT price FROM Rooms)                         -- все ли комнаты дешевле чем 200
+SELECT name FROM Users JOIN Rooms ON Users.id = Rooms.owner_id WHERE Users.id <> ALL (SELECT user_id FROM Res) -- в многотабличном запросе с таблицей не объединенной с ним
+
+-- IN - проверяет входит ли конкретное значение в набор значений (тут подзвпроса возвращающего набор строк одного столбца).
+SELECT id, name FROM departments WHERE id IN (SELECT department_id FROM sales WHERE price > 98)  -- так как используется IN то в подзапросе должен быть 1 стобец
+SELECT good_name FROM Goods WHERE good_id NOT IN (SELECT good FROM Payments)                     -- NOT IN
+
+-- ANY - имеет схожие поведение c IN но возвращает TRUE(те работает через операторы сравнения), если хотя бы одно сравнение отдельного значения со значением в наборе вернёт TRUE.
+SELECT * FROM Users WHERE id = ANY (SELECT DISTINCT owner_id FROM Rooms WHERE price >= 150)  -- найдёт пользователей, которые владеют хотя бы 1 жилым помещением стоимостью более 150
+
+
+
+--                                     1-в. Многостолбцовые подзапросы
+
+-- Многостолбцовые подзапросы - возвращают несколько столбцов и несколько строк (производные таблицы)
+SELECT * FROM Reservations WHERE (room_id, price) IN (SELECT id, price FROM Rooms)     -- попарно сравнивает значения в основном запросе со значениями в подзапросе
+SELECT * FROM Rooms WHERE (has_tv, has_internet) IN (SELECT has_tv, has_internet FROM Rooms WHERE id=11)  -- по одной конкретной строке
+
+
+
+--                                       2. Коррелированные подзапросы
+
+-- Коррелированные подзапросы - ссылаются на один или несколько столбцов основного запроса
+-- Коррелированные подзапросы зависимые(все прошлые 3 варианта были независимы от остального запроса) - он выполняется не один раз перед выполнением запроса, в который он вложен, а для каждой строки, которая может быть включена в окончательный результат. Потому использование коррелированных подзапросов может вызвать проблемы с производительностью, особенно если содержащий запрос возвращает много строк
+
 SELECT
-   FamilyMembers.member_name,
-   (SELECT MAX(Payments.unit_price) FROM Payments WHERE Payments.family_member = FamilyMembers.member_id) AS max_price
-FROM FamilyMembers
---#=> Выводим для каждой строки максимум значения колонки из строки другой таблицы по соотв этих строк в значениях Payments.family_member и FamilyMembers.member_id
+  Family.name,
+  (SELECT MAX(Payments.price) FROM Payments WHERE Payments.member = Family.member_id) AS max_price
+FROM Family
+-- По Family.member_id находим значения в другой таблице соответсвующие данной строке в базовом запросе
 
 -- Можно использовать другой запрос после FROM вместо имени таблицы
-select user_id, video_id from (select user_id, video_id, count(distinct video_id) over (partition by user_id) as cnt from user_playlist) t
+SELECT user_id, video_id
+FROM (SELECT user_id, video_id, COUNT(distinct video_id) OVER (PARTITION BY user_id) AS cnt FROM user_playlist) t
 
 -- Подзапрос таблицы к самой себе. Имитирует GROUP BY по полю supplier_id
 SELECT DISTINCT ON(supplier_id)
 supplier_id,
 (SELECT COUNT(id) FROM products AS o WHERE o.supplier_id = p.supplier_id) AS total_products
 FROM products AS p
-ORDER BY supplier_id DESC
 
-	-- EXISTS используется в сочетании с подзапросом и считается выполненным, если подзапрос возвращает хотя бы одну строку
-	-- (Операторы SQL, использующие условие EXISTS, очень неэффективны, поскольку подзапрос повторно запускается для КАЖДОЙ строки в таблице внешнего запроса)
+
+-- EXISTS используется в сочетании с подзапросом и считается выполненным, если подзапрос возвращает хотя бы одну строку (Операторы SQL, использующие условие EXISTS, очень неэффективны, поскольку подзапрос повторно запускается для КАЖДОЙ строки в таблице внешнего запроса)
 SELECT * FROM customers WHERE EXISTS (SELECT * FROM orders WHERE customers.customer_id = orders.customer_id)
 
-	-- Двойное условие для подзапроса(не только для EXISTS а для любого оператора)
-SELECT * FROM dep WHERE EXISTS (SELECT dep_id FROM sales WHERE sales.dep_id = dep.id AND price > 98)          --#=> 2е условие для позапроса через AND
 
-	-- EXCEPT  - не соответсвие подзапросу
+-- EXCEPT  - не соответсвие подзапросу
 SELECT id FROM orders EXCEPT SELECT id FROM orders WHERE date IS NOT NULL
 
 
--- 14_uslovn.sql
+++ 14_uslovn.sql
@@ -1,66 +1,94 @@
---                                           Условная логика
+--                                   CASE (условный оператор/условная логика)
 
--- CASE (условный оператор/условная логика) - результат обычно представляет собой значение какогото нового выводимого столбца
-SELECT
-name,
-CASE
- WHEN SUBSTRING(name, 1, INSTR(name, ' ')) IN (10, 11) THEN "Старшая школа"
- WHEN SUBSTRING(name, 1, INSTR(name, ' ')) IN (5, 6, 7, 8, 9) THEN "Средняя школа"
- ELSE "Начальная школа"
-END AS stage     --#=>   возвращаем в новую колонку stage значения в зависимости от условия
-FROM Class        --#=>   несколько условий(если первое не срабатывает переходим ко второму итд)
+-- CASE - результат обычно представляет собой значение какогото нового выводимого столбца
 
 SELECT
-CASE
-  WHEN SUM(number1)%2=0 THEN MAX(number1)
-  ELSE MIN(number1)
-END AS number1,
-CASE
-  WHEN SUM(number2)%2=0 THEN MAX(number2)
-  ELSE MIN(number2)
-END AS number2
-FROM numbers      --#=>    несколько условных операторов через запятую
+  name,
+  CASE             --  несколько условий(если первое не срабатывает переходим ко второму итд)
+    WHEN SUBSTRING(name, 1, INSTR(name, ' ')) IN (10, 11) THEN "Старшая школа"
+    WHEN SUBSTRING(name, 1, INSTR(name, ' ')) IN (5, 6, 7, 8, 9) THEN "Средняя школа"
+    ELSE "Начальная школа"
+  END AS stage     --  возвращаем в новую колонку stage значения в зависимости от условия
+FROM Class
 
-SELECT name,
-CASE SUBSTRING(name, 1, INSTR(name, ' '))
-  WHEN 11 THEN "Старшая школа"
-  WHEN 9 THEN "Средняя школа"
-  ELSE "Начальная школа"
-END AS stage
-FROM Class        --#=>   просто стравниваем значения CASE со значениями в WHEN по одному(придется писать много условий для каждого класса)
+-- Запись сравниваемого значения после CASE, подходит если сравнивает только с 1м значением в условии
+SELECT
+  name,
+  CASE SUBSTRING(name, 1, INSTR(name, ' '))
+    WHEN 11 THEN "Старшая школа"
+    WHEN 9 THEN "Средняя школа"
+    ELSE "Начальная школа"
+  END AS stage
+FROM Class
 
+-- Несколько условных операторов в запросе записываются через запятую
+SELECT
+  CASE
+    WHEN SUM(number1)%2=0 THEN MAX(number1)
+    ELSE MIN(number1)
+  END AS number1,
+  CASE
+    WHEN SUM(number2)%2=0 THEN MAX(number2)
+    ELSE MIN(number2)
+  END AS number2
+FROM numbers
+
+-- В одну строку с надфункцией(какойто странный пример ??)
 SELECT p.name,
-COUNT(CASE WHEN d.detail LIKE '%oo%' THEN 1 ELSE 0 END) AS good,
-COUNT(CASE WHEN d.detail = 'ok'  THEN 1 ELSE 0 END) AS ok,
-COUNT(CASE WHEN d.detail = 'bad' THEN 1 ELSE 0 END) AS bad
-FROM products p
-INNER JOIN details d ON p.id = d.product_id  --#=> в одну строку с надфункцией
+  COUNT(CASE WHEN d.detail LIKE '%oo%' THEN 1 ELSE 0 END) AS good,
+  COUNT(CASE WHEN d.detail = 'ok'  THEN 1 ELSE 0 END) AS ok,
+  COUNT(CASE WHEN d.detail = 'bad' THEN 1 ELSE 0 END) AS bad
+FROM products p JOIN details d ON p.id = d.product_id
 
 
 
--- IF (условная функция/условная логика) - IF (условное_выражение, значение_1, значение_2);
--- (Если условное выражение в первом аргументе в функции IF, истинно, функция вернёт значение второго аргумента значение_1, иначе возвращается значение третьего аргумента значение_2)
-SELECT IF(10 > 20, "TRUE", "FALSE");                                                          --#=> "FALSE"
-SELECT id, price, IF(price >= 150, "Комфорт-класс", "Эконом-класс") AS category FROM Rooms    --#=> ставим значеня в колонке category в зависимости от значения колонки price
-SELECT id, price, IF(price >= 200, "Бизнес-класс", IF(price >= 150, "Комфорт-класс", "Эконом-класс")) AS category FROM Rooms   --#=> вложение одного IF в другой(эмуляция CASE)
+--                                     IF (условная функция/условная логика)
 
--- IFNULL (условная функция) - IFNULL(значение, альтернативное_значение); возвращает значение, переданное первым аргументом, если оно не равно NULL, иначе возвращает альтернативное_значение
-SELECT IFNULL("SQL Academy", "Альтернатива SQL Academy") AS sql_trainer;
+-- IF (условное_выражение, значение_1, значение_2). Если условное выражение в первом аргументе в функции IF, истинно, функция вернёт значение второго аргумента значение_1, иначе возвращается значение третьего аргумента значение_2.
 
--- NULLIF (условная функция) - NULLIF(значение_1, значение_2); возвращает NULL, если значение_1 равно значению_2, в противном случае возвращает значение_1(тоесть колонку для проверки ставим в 1)
-SELECT NULLIF("SQL Academy", "SQL Academy") AS sql_trainer;                                   --#=> <NULL>
-SELECT NULLIF("SQL Academy", "Альтернатива SQL Academy") AS sql_trainer;                      --#=> "SQL Academy"
+SELECT IF(10 > 20, "TRUE", "FALSE");                                          --> "FALSE"
+SELECT price, IF(price >= 150, "Комфорт", "Эконом") AS category FROM Rooms    -- ставим значеня в зависимости от price
+SELECT price, IF(price >= 200, "Бизнес", IF(price >= 150, "Комфорт", "Эконом")) AS category FROM Rooms   -- вложение одного IF в другой(эмуляция CASE)
+
+
+
+--                                          IFNULL (условная функция)
+
+-- IFNULL(значение, альтернативное_значение) - возвращает значение, переданное первым аргументом, если оно не равно NULL, иначе возвращает альтернативное_значение
+SELECT IFNULL("SQL Academy", "Альтернатива SQL Academy") AS sql_trainer;     --> "SQL Academy"
+SELECT IFNULL(some, "Альтернатива SQL Academy") AS sql_trainer;
+
+
+
+--                                          NULLIF (условная функция)
+
+-- NULLIF(значение_1, значение_2) - возвращает NULL, если значение_1 равно значению_2, в противном случае возвращает значение_1(тоесть колонку для проверки ставим в 1)
+SELECT NULLIF("SQL Academy", "SQL Academy") AS sql_trainer;                 --> <NULL>
+SELECT NULLIF("SQL Academy", "Альтернатива SQL Academy") AS sql_trainer;    --> "SQL Academy"
+
+
+
+--                                                  COALESCE
 
 -- COALESCE - это специальное выражение, которое вычисляет по порядку каждый из своих аргументов и на выходе возвращает значение первого аргумента, который был не NULL.
-SELECT COALESCE(NULL, NULL, 1, 2, NULL, 3)                                                    --#=> 1
-SELECT name, COALESCE(bonus1, bonus2, 1000000) AS bonus FROM table_name                       --#=> если bonus1==NULL выбирает значение bonus2 если и оно ==NULL, тогда выбирает 1000000
+SELECT COALESCE(NULL, NULL, 1, 2, NULL, 3)                                 --> 1
+SELECT name, COALESCE(bonus1, bonus2, 1000000) AS bonus FROM table_name    -- если bonus1 == NULL выбирает значение bonus2 если и оно == NULL, тогда выбирает 1000000
 
-SELECT COALESCE(NULLIF(name,''), '[product name not found]') AS name FROM eusales             --#=> при помощи NULLIF меняем пустые строки на NULL чтобы применить COALESCE
+SELECT COALESCE(NULLIF(name, ''), '[product name not found]') AS name FROM eusales  -- при помощи NULLIF меняем пустые строки на NULL чтобы применить COALESCE
 
 
--- [постгресс] bool_or(column = value)  Если значение равно значению столбца вернет true иначе false
-select username, bool_or(role = 'internal') as internal, bool_or(role = 'admin') as admin from user_roles
 
+--                                      [постгресс] BOOL_OR
+
+-- [постгресс] BOOL_OR(column = value)  Если значение равно значению столбца вернет true иначе false
+SELECT username, BOOL_OR(role = 'internal') AS internal, BOOL_OR(role = 'admin') AS admin FROM user_roles
+
+
+
+--                                     [постгресс ??] Условие без оператора
+
+-- Условие возврвщающее тру или фолс можно писать без оператора
+SELECT id, n % x = 0 AND n % y = 0 AS res FROM kata
 
 
 
