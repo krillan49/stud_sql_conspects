5bd00f7c66ecd1658ee0c5a22e2c4c6086a3a228
krillan <pushkinidze49@gmail.com>
2024-03-16 22:07:16 +0300

    hgfhgfjhgff

--- 19_DELETE_TRUNCATE.sql
+++ 19_DELETE_TRUNCATE.sql
@@ -1,11 +1,11 @@
---                                       DELETE и TRUNCATE
+--                                          DELETE и TRUNCATE
 
 -- DELETE и TRUNCATE  -  операторы удаления записей из таблицы. Наиболее универсальным и безопасным является DELETE
 
 DELETE FROM имя_таблицы WHERE условие_отбора_записей;
 -- Если условие отбора записей WHERE отсутствует, то будут удалены все записи указанной таблицы. Записи удаляются по одной.
 -- Оптимизатор запросов СУБД MySQL автоматически использует оператор TRUNCATE, если оператор DELETE не содержит условия WHERE или конструкции LIMIT.
-DELETE FROM test WHERE id > 10                     -- удаляем все строки где значение id больше 2
+DELETE FROM test WHERE id > 10                     -- удаляем все строки где значение id больше 10
 
 
 TRUNCATE TABLE имя_таблицы;
@@ -16,13 +16,15 @@ TRUNCATE TABLE имя_таблицы;
 -- Чтобы использовать, необходимы права на изменение таблицы
 
 
--- Удаление записей при многотабличных запросах
+
+--                               Удаление записей при многотабличных запросах
+
 -- Если в DELETE запросе используется JOIN, то необходимо указать из каких(ой) именно таблиц(ы) требуется удалять записи
 DELETE имя_таблицы_1 [, имя_таблицы_2]
 FROM имя_таблицы_1 JOIN имя_таблицы_2 ON имя_таблицы_1.поле = имя_таблицы_2.поле [WHERE условие_отбора_записей];
 
 -- Между операторами DELETE и FROM пишем таблицы из которых хотим удалить записи соответсвующие условию
-DELETE Res FROM Res JOIN Rooms ON Res.room_id = Rooms.id WHERE Rooms.has_kitchen = false;	-- удаляем только из таблицы Res
+DELETE Res FROM Res JOIN Rooms ON Res.room_id = Rooms.id WHERE Rooms.has_kitchen = false;	        -- удаляем только из таблицы Res
 DELETE Res, Rooms FROM Res JOIN Rooms ON Res.room_id = Rooms.id WHERE Rooms.has_kitchen = false;  -- удаляем из 2х таблиц
 
 
