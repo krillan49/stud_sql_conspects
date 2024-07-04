49f9c2fe52bb51ebfd59ac97e3083240c3c6ffce
krillan <pushkinidze49@gmail.com>
2024-07-04 10:26:15 +0300

    sqlfb l3 29-13

--- 14_UNION.sql
+++ 14_UNION.sql
@@ -1,3 +1,6 @@
+-- Операции над множествами
+
+
 --                                              UNION
 
 -- UNION (Объединение запросов) - объединяет запросы в одну результирующую таблицу.
@@ -6,23 +9,47 @@
 -- Чтобы UNION корректно сработал нужно чтобы результирующие таблицы каждого из SQL запросов имели одинаковое число столбцов, с одним и тем же типом данных и в той же самой последовательности.
 
 SELECT good_name AS name FROM Goods
-UNION 
+UNION
 SELECT member_name AS name FROM Family;
 -- в один столбец name попадут и имена членов семьи и названия товаров
 
+-- UNION ALL не убирает дубликаты(не уберет дубликаты даже если добавить дистинкт в каждый запрос)
 SELECT first_name, middle_name, last_name FROM Student
-UNION
+UNION ALL
 SELECT first_name, middle_name, last_name FROM Teacher;
 -- объединяем по столбцам ФИО строки данных учеников и учителей
 
 
--- UNION ALL не убирает дубликаты
+-- INTERSECT (Пересечение запросов) Комбинирует два запроса SELECT, но возвращает только совпадающие записи первого и второго SELECT
+
+SELECT country FROM customers
+INTERSECT
+SELECT country FROM suppliers
 
 
--- INTERSECT Комбинирует два запроса SELECT, но возвращает записи только первого SELECT, которые имеют совпадения во втором элементе SELECT.(работает как UNION ??)
+-- EXCEPT (Исключение/разница запросов) Комбинирует два запроса SELECT, но возвращает только те записи первого SELECT, которые не имеют совпадения во втором элементе SELECT. Тоесть есть разница какой запрос ставить 1м. По умолчанию накладывает дистинкт на 1й запрос
+
+SELECT country
+FROM customers
+EXCEPT
+SELECT country
+FROM suppliers
+
+-- EXCEPT ALL  - дистинкт не наложен ни на 1 запрос потому возвращает только те записи которых нет в том запросе, в котором есть меньше дубликатов этой записи. Тоесть большее число дубликатов минус меньшее
+SELECT country
+FROM customers
+EXCEPT ALL
+SELECT country
+FROM suppliers
 
 
--- EXCEPT Комбинирует два запроса SELECT, но возвращает записи только первого SELECT, которые не имеют совпадения во втором элементе SELECT.(работает как UNION ??)
+
+-- Этими операциями над множествами можно объединять более 2х запросов
+SELECT country FROM customers
+INTERSECT
+SELECT country FROM suppliers
+EXCEPT
+SELECT country FROM employees
 
 
 
